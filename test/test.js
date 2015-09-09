5b231e3f8413b6ba7339ee1f4490bcb3d6f7a7f5
Rafael Nowrotek <mail@benignware.com>
2015-09-09 18:10:24 +0200

    Added output html

--- test/test.js
+++ test/test.js
@@ -1,15 +1,13 @@
 /**
  * DOM Tests
  */
-
 asyncTest("transform", function(assert) {
-  QUnit.start();
   $.ajax({
     url: 'fixtures/test.xml',
     dataType: 'xslt'
   }).done(function(data) {
+    $('body > .container').append($(data).find('body').html());
     var expected = '<html><head><meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/><title>Hello World!</title></head><body><h1>Hello World!</h1></body></html>';
     assert.domEqual(data.documentElement, expected);
   });
-  
 });
