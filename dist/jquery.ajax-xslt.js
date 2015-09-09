a0216ff8b1d4297be5bf172adb7def51dce30cf2
Rafael Nowrotek <mail@benignware.com>
2015-09-09 18:11:33 +0200

    Updated build

--- dist/jquery.ajax-xslt.js
+++ dist/jquery.ajax-xslt.js
@@ -13,7 +13,7 @@
     for (i = 0; i < children.length; i++) {
       child = children[i];
       if (child.target === 'xml-stylesheet') {
-        match = child.data.match(/href=\"(.*)\"/);
+        match = child.data.match(/href=\"([^'"]*)\"/);
         if (match) {
           file = match[1];
           // Check for relative path
@@ -28,11 +28,57 @@
     return file;
   }
   
+  function serializeToString(xml) {  
+    if (window.XMLSerializer) {
+      // W3C
+      var serializer = new XMLSerializer();
+      var string = serializer.serializeToString(xml);
+      return string;
+    } else if (typeof xml.xml === 'object') {
+      // Internet Explorer 8
+      return xml.xml;
+    }  
+  }
+  
+  
+  // Based on https://gist.github.com/1129031 by Eli Grey, http://eligrey.com
+  function parseFromString(xmlString, outputType) {
+    var
+      doc;
+    
+    // Firefox/Opera/IE throw errors on unsupported types
+    try {
+      doc = (new DOMParser()).parseFromString(xmlString, outputType);
+      if (doc) {
+        // Output type is natively supported
+        return doc;
+      }
+    } catch (ex) {
+    }
+    
+    // HTML-Extension
+    if (/^\s*text\/html\s*(?:;|$)/i.test(outputType)) {
+      doc = document.implementation.createHTMLDocument("");
+      if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+        doc.documentElement.innerHTML = markup;
+      }
+      else {
+        doc.body.innerHTML = markup;
+      }
+      return doc;
+    }
+    
+    // Unsupported output type
+    return null;
+  }
+  
   function transformToDocument(xml, xsl, options) {
+    
     options = options || {};
     var result = null, xsltProcessor;
-    // Transform
-    if (xml.implementation && xml.implementation.createDocument) {
+    
+    if ('XSLTProcessor' in window && xml.implementation && xml.implementation.createDocument) {
+      
       // W3C
       xsltProcessor = new XSLTProcessor();
       try {
@@ -41,37 +87,78 @@
         console.error(e);
       }
       result = xsltProcessor.transformToDocument(xml);
-    } else {
-      // Internet Explorer
-      var impl = new ActiveXObject("Microsoft.XMLDOM");
-      if (impl) {
-        var xslt = new ActiveXObject("MSXML2.FreeThreadedDOMDocument");
-        var processor   = new ActiveXObject("Msxml2.XSLTemplate");
-        processor.stylesheet = xsl;
-        var objXSLTProc = processor.createProcessor();
-        objXSLTProc.input = xml;
-        objXSLTProc.transform();
-        result = objXSLTProc.output;
+      
+    } else if ('DOMParser' in window && (window.ActiveXObject || "ActiveXObject" in window)) {
+      
+      // Internet Explorer 9+
+      try {
+        
+        var
+          // Resolve output type
+          outputElement = xsl.getElementsByTagNameNS('http://www.w3.org/1999/XSL/Transform', 'output')[0],
+          outputMethod = outputElement && outputElement.getAttribute('method') || 'xml',
+          outputType = outputMethod === 'html' ? 'text/html' : 'text/xml',
+        
+          // Serialize string
+          xmlString = serializeToString(xml);
+        
+        // Replace entities
+        // TODO: Configurable as option
+        xmlString = xmlString.replace(/\&shy;/gi, '&#173;');
+        
+        var xmlDoc = new ActiveXObject("Msxml2.DOMDocument");
+        xmlDoc.async = false;
+        xmlDoc.resolveExternals = false;
+        xmlDoc.validateOnParse = false;
+        xmlDoc.loadXML(xmlString);
+        
+        var
+          xslDoc = new ActiveXObject("Msxml2.FreeThreadedDOMDocument"),
+          xslString = serializeToString(xsl);
+        
+        xslDoc.loadXML(xslString);
+        
+        var output;
+        if ('transformNode' in xmlDoc) {
+          output = xmlDoc.transformNode(xslDoc);
+        }
+        
+        if (!output) {
+          var xslt = new ActiveXObject("Msxml2.XSLTemplate");
+          xslt.stylesheet = xslDoc;
+          xsltProcessor = xslt.createProcessor();
+          xsltProcessor.input = xmlDoc;
+          xsltProcessor.transform();
+          output = xsltProcessor.output;
+        }
+        
+        result = parseFromString( output , outputType );
+        
+      } catch (e) {
+        console.error(e);
       }
+    } else {
+      // Unsupported
+      console.warn("XSLT is not supported");
     }
-    
-    function unescapeOutput(result) {
-      $(result).find('*').contents().filter(function () { return this.nodeType === 3; }).each(function() {
-        var frag = document.createDocumentFragment();
-        var holder = document.createElement("div");
-        holder.innerHTML = this.nodeValue;
-        $(holder).contents().each(function() {
-          frag.appendChild(this);
-        });
-        $(frag).insertBefore(this);
-        $(this).remove();
-      });
-    }
-    
     return result;
   }
   
+  function unescapeOutput(result) {
+    $(result).find('*').contents().filter(function () { return this.nodeType === 3; }).each(function() {
+      var frag = document.createDocumentFragment();
+      var holder = document.createElement("div");
+      holder.innerHTML = this.nodeValue;
+      $(holder).contents().each(function() {
+        frag.appendChild(this);
+      });
+      $(frag).insertBefore(this);
+      $(this).remove();
+    });
+  }
+  
   $.ajaxTransport("xslt", function(options, originalOptions, jqXHR, headers, completeCallback   ) {
+    
     if (options.dataType === 'xslt' || options.xmlStylesheet) {
       options.dataType = 'xslt';
       var xhr, aborted = false;
@@ -80,6 +167,7 @@
           // Load Document
           xhr = $.ajax($.extend(true, {}, options, {
             dataType: 'xml',
+            contentType: 'application/xml',
             success: function(xml) {
               if (xml && !aborted) {
                 var xmlStylesheet = options.xmlStylesheet || getXMLStylesheet(options.url, xml);
@@ -88,6 +176,7 @@
                   xhr = $.ajax({
                     url: xmlStylesheet,
                     dataType: 'xml',
+                    contentType: 'text/xml',
                     success: function(xmlStylesheet) {
                       if (!aborted) {
                         // Transform
